790c6b898d45af8735d2a078a8c5f186a3b58350
Anand Kumar <anand@grootmart.com>
2012-01-06 02:55:14 +0530

    Function Definiton started giving....check for it correctness ;)

--- application/models/mail.php
+++ application/models/mail.php
@@ -6,37 +6,75 @@
  */
 
 /**
- * Description of mail
+ * Class file which contains all the functions related to the mail funcationality which can be used
+ * to sending mails to the user.
  *
- * @author lucky
+ * @author Anand Kumar Chaudhary
  */
 class Login extends CI_Model{
-        var $from='no-reply@grootmart.com';    
+        var $from='no-reply@grootmart.com';
+        var $user_id=NULL;
+        var $to=NULL;
+        var $message='Hi';
+        var $dbc=NULL;
+        var $sender_name='GrootMart.com';
+        var $sub='';
+        var $code=NULL;
+
     
     
-	function __construct(){
-	parent::__construct();
-        $this->load->database();
-        $this->load->library('email');
+	function __construct($user_id){
+            
+            //Validation to check that the Deliverd user_id is NUMERIC
+            if(is_numeric($user_id))
+           {
+                $this->user_id = $user_id;
+                
+                
+                }
+           
+            parent::__construct();
+            $this->dbc=$this->load->database('',TRUE);
+            
+            if(!is_null($user_id)){
+                 
+                $this->dbc->select('email');
+                
+                $this->to = $this->dbc->get_where('User', array('ID' =>$this->user_id));
+                
+            }
+             $this->load->library('email');
         
 		}
         // Functio to send validation mail to the user after the registration
-                function send_validation_mail($user_id){
+                function send_validation_mail(){
+                    
+                    $this->sub="Email Verification";
+                    $this->email->subject($this->sub);
+                    $this->email->from($this->from,$this->sender_name);
+                    $this->email->to($this->to);
+                    $this->code=md5($this->user_id.time());
+                    
+                    
+                    
+                    
+                    
+                    
                     
                 }
         // Function to send mail to confirm that user has been validated
-                function user_validated($user_id,$secret_code){}
+                function user_validated(){}
         //Function to send that users password is updated
-                function password_changed($user_id){}
+                function password_changed(){}
         
          //function to send out password_reset_link
-                function password_reset_link($user_id){}
+                function password_reset_link(){}
          
         //function to send message that order has been placed
-        function order_placed_mail($param){} 
+        function order_placed_mail(){} 
         
         //function to mail about order status e.g DELIVERD OF DEFFERED
-        function order_status_update_mail($param) {}
+        function order_status_update_mail() {}
         
        
         
