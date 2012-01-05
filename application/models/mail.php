a24528230cf8902700e2a793fd8c9eae946b1f5f
Anand Kumar <anand@grootmart.com>
2012-01-06 01:36:40 +0530

    Parameters Added to the newly created functions

--- application/models/mail.php
+++ application/models/mail.php
@@ -21,24 +21,24 @@ class Login extends CI_Model{
         
 		}
         // Functio to send validation mail to the user after the registration
-                function send_validation_mail(){
+                function send_validation_mail($user_id){
                     
                 }
         // Function to send mail to confirm that user has been validated
-                function user_validated(){}
+                function user_validated($user_id,$secret_code){}
         //Function to send that users password is updated
-                function password_changed(){}
+                function password_changed($user_id){}
         
          //function to send out password_reset_link
-                function password_reset_link(){}
+                function password_reset_link($user_id){}
          
         //function to send message that order has been placed
         function order_placed_mail($param){} 
         
-        //function to mail about order status
+        //function to mail about order status e.g DELIVERD OF DEFFERED
         function order_status_update_mail($param) {}
         
-        //function to get the
+       
         
             
     
